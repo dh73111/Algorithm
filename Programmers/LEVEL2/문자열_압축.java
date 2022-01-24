36217dc25ee493b57ebd215ac723faf1c45ddca9
DooHee <dh73111@gmail.com>
2022-01-24 22:48:24 +0900

    Programmers : LEVEL2  문자열 압축

---
+++ Programmers/LEVEL2/문자열_압축.java
@@ -0,0 +1,45 @@
+package programmers;
+
+public class 문자열_압축 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "aabbaccc";
+		solution(s);
+	}
+
+	public static int solution(String s) {
+		int answer = s.length();
+
+        for (int i = 1; i < s.length() / 2 + 1; i++) {
+            String prev = s.substring(0, i);
+            int count = 1;
+            String enc = "";
+            String last = "";
+            for (int j = i; j < s.length(); j += i) {
+                if (j + i > s.length()) {
+                    last = s.substring(j);
+                    continue;
+                }
+                if (prev.equals(s.substring(j, j + i))) {
+                    count++;
+                } else {
+                    enc += prev;
+                    if (count != 1) {
+                        enc = count + enc;
+                    }
+                    prev = s.substring(j, j + i);
+                    count = 1;
+                }
+            }
+            enc += prev + last;
+            if (count != 1) {
+                enc = count + enc;
+            } 
+            
+            answer = Math.min(answer, enc.length());
+        }
+
+        return answer;
+    }
+}
